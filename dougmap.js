a0cfff9318ae5ba12897bb58e6ce33df3d76394e
Doug Banville <doug.banville@rte.ie>
2018-09-28 16:52:54 +0100

    map showing

---
+++ dougmap.js
@@ -0,0 +1,122 @@
+var ALL_CONSTITUENCIES = {
+    'C01': {
+        'CONSTITUENCY_NAME': 'Carlow-Kilkenny'
+    },
+    'C02': {
+        'CONSTITUENCY_NAME': 'Cavan-Monaghan'
+    },
+    'C03': {
+        'CONSTITUENCY_NAME': 'Clare'
+    },
+    'C04': {
+        'CONSTITUENCY_NAME': 'Cork East'
+    },
+    'C05': {
+        'CONSTITUENCY_NAME': 'Cork North-Central'
+    },
+    'C06': {
+        'CONSTITUENCY_NAME': 'Cork North-West'
+    },
+    'C07': {
+        'CONSTITUENCY_NAME': 'Cork South-Central'
+    },
+    'C08': {
+        'CONSTITUENCY_NAME': 'Cork South-West'
+    },
+    'C09': {
+        'CONSTITUENCY_NAME': 'Donegal'
+    },
+    'C10': {
+        'CONSTITUENCY_NAME': 'Dublin Bay North'
+    },
+    'C11': {
+        'CONSTITUENCY_NAME': 'Dublin Bay South'
+    },
+    'C12': {
+        'CONSTITUENCY_NAME': 'Dublin Central'
+    },
+    'C13': {
+        'CONSTITUENCY_NAME': 'Dublin Fingal'
+    },
+    'C14': {
+        'CONSTITUENCY_NAME': 'Dublin Mid-West'
+    },
+    'C15': {
+        'CONSTITUENCY_NAME': 'Dublin North-West'
+    },
+    'C16': {
+        'CONSTITUENCY_NAME': 'Dublin Rathdown'
+    },
+    'C17': {
+        'CONSTITUENCY_NAME': 'Dublin South-Central'
+    },
+    'C18': {
+        'CONSTITUENCY_NAME': 'Dublin South-West'
+    },
+    'C19': {
+        'CONSTITUENCY_NAME': 'Dublin West'
+    },
+    'C20': {
+        'CONSTITUENCY_NAME': 'Dún Laoghaire'
+    },
+    'C21': {
+        'CONSTITUENCY_NAME': 'Galway East'
+    },
+    'C22': {
+        'CONSTITUENCY_NAME': 'Galway West'
+    },
+    'C23': {
+        'CONSTITUENCY_NAME': 'Kerry'
+    },
+    'C24': {
+        'CONSTITUENCY_NAME': 'Kildare North'
+    },
+    'C25': {
+        'CONSTITUENCY_NAME': 'Kildare South'
+    },
+    'C26': {
+        'CONSTITUENCY_NAME': 'Laois'
+    },
+    'C27': {
+        'CONSTITUENCY_NAME': 'Limerick City'
+    },
+    'C28': {
+        'CONSTITUENCY_NAME': 'Limerick County'
+    },
+    'C29': {
+        'CONSTITUENCY_NAME': 'Longford-Westmeath'
+    },
+    'C30': {
+        'CONSTITUENCY_NAME': 'Louth'
+    },
+    'C31': {
+        'CONSTITUENCY_NAME': 'Mayo'
+    },
+    'C32': {
+        'CONSTITUENCY_NAME': 'Meath East'
+    },
+    'C33': {
+        'CONSTITUENCY_NAME': 'Meath West'
+    },
+    'C34': {
+        'CONSTITUENCY_NAME': 'Offaly'
+    },
+    'C35': {
+        'CONSTITUENCY_NAME': 'Roscommon-Galway'
+    },
+    'C36': {
+        'CONSTITUENCY_NAME': 'Sligo-Leitrim'
+    },
+    'C37': {
+        'CONSTITUENCY_NAME': 'Tipperary'
+    },
+    'C38': {
+        'CONSTITUENCY_NAME': 'Waterford'
+    },
+    'C39': {
+        'CONSTITUENCY_NAME': 'Wexford'
+    },
+    'C40': {
+        'CONSTITUENCY_NAME': 'Wicklow'
+    }
+};
